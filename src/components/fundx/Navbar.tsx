b2057f70cb9c138e5cf059a500a6417c7ce3a4a1
jadonamite <jadonamite@gmail.com>
2026-03-29 17:19:02 +0100

    fix: fix issue in ui (gitignore, Navbar) (improvement)

--- src/components/fundx/Navbar.tsx
+++ src/components/fundx/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import Logo from "@/components/Logo"
-import { ConnectWallet } from "@/components/fundx/ConnectWallet"
-import { useStacks } from "@/components/fundx/StacksProvider"
+import Logo from "@/components_/Logo"
+import { ConnectWallet } from "@/components_/fundx/ConnectWallet"
+import { useStacks } from "@/components_/fundx/StacksProvider"
 
 export function Navbar() {
   // Pull in the connection status to know if we should show the Dashboard link
