12bffaccfd982b60c1b04e03731221932ac7c2b9
jadonamite <jadonamite@gmail.com>
2026-03-29 21:50:58 +0100

    refactor: minor tweaks (cleanup)

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { toast } from "sonner" // <--- Import Toast
-import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
 import { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
+import { useStacks } from "@/components/fundx/StacksProvider"
+import { 
   DropdownMenu, 
   DropdownMenuContent, 
   DropdownMenuItem, 
@@ -11,8 +11,8 @@ import { Button } from "@/components/ui/button"
   DropdownMenuSeparator, 
   DropdownMenuTrigger 
 } from "@/components/ui/dropdown-menu"
-import { useStacks } from "@/components/fundx/StacksProvider"
-import { 
+import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
+import { toast } from "sonner" // <--- Import Toast
 
 export function ConnectWallet() {
   const { authenticate, signOut, isSignedIn, walletData } = useStacks()
