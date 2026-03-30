acae29b09b51c48c01d536bcac2f776ba8385409
jadonamite <jadonamite@gmail.com>
2026-03-30 10:00:04 +0100

    fix: correct ui handling (progress)

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import {
+import { useTheme } from "next-themes"
   CircleCheckIcon,
   InfoIcon,
   Loader2Icon,
   OctagonXIcon,
   TriangleAlertIcon,
 } from "lucide-react"
+import {
 import { Toaster as Sonner, type ToasterProps } from "sonner"
-import { useTheme } from "next-themes"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
