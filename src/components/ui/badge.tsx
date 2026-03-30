21e84f5c3a6c7b02bb68c469e4c9c499f0d32e52
jadonamite <jadonamite@gmail.com>
2026-03-30 11:49:41 +0100

    refactor: simplify ui & logic logic (progress)

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,7 +8,7 @@ const badgeVariants = cva(
   "inline-flex items-center justify-center rounded-full border border-transparent px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
-      variant_: {
+      variant: {
         default: "bg-gradient-tush text-primary-foreground [a&]:hover:bg-gradient-tush/90",
         secondary:
           "bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
@@ -21,14 +21,14 @@ const badgeVariants = cva(
       },
     },
     defaultVariants: {
-      variant_: "default",
+      variant: "default",
     },
   }
 )
 
 function Badge({
   className,
-  variant_ = "default",
+  variant = "default",
   asChild = false,
   ...props
 }: React.ComponentProps<"span"> &
@@ -38,8 +38,8 @@ function Badge({
   return (
     <Comp
       data-slot="badge"
-      data-variant_={variant_}
-      className={cn(badgeVariants({ variant_ }), className)}
+      data-variant={variant}
+      className={cn(badgeVariants({ variant }), className)}
       {...props}
     />
   )
