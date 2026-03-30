2a6449c3e295e788743050cb737d8dbd30b13f6c
jadonamite <jadonamite@gmail.com>
2026-03-30 10:41:36 +0100

    refactor: improve logic structure (progress)

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { 
+import { Button } from "@/components/ui/button"
 import { useStacks } from "@/components/fundx/StacksProvider"
-import { toast } from "sonner" // <--- Import Toast
+import { 
   DropdownMenu, 
   DropdownMenuContent, 
   DropdownMenuItem, 
@@ -11,8 +11,8 @@ import { toast } from "sonner" // <--- Import Toast
   DropdownMenuSeparator, 
   DropdownMenuTrigger 
 } from "@/components/ui/dropdown-menu"
-import { Button } from "@/components/ui/button"
 import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
+import { toast } from "sonner" // <--- Import Toast
 
 export function ConnectWallet() {
   const { authenticate, signOut, isSignedIn, walletData } = useStacks()
--- src/components/fundx/Footer.tsx
+++ src/components/fundx/Footer.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import Logo from "@/components/Logo"
+import { Input } from "@/components/ui/input"
 
 export function Footer() {
   return (
