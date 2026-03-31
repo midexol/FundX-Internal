51ae89b77b76af6b9da38c9452eebf51eecce05c
jadonamite <jadonamite@gmail.com>
2026-03-31 17:09:51 +0100

    fix: fix issue in ui (improvement)

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -14,14 +14,14 @@ function Progress({
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "bg-gradient-tush_/20 relative h-2 w-full overflow-hidden rounded-full",
+        "bg-gradient-tush/20 relative h-2 w-full overflow-hidden rounded-full",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-gradient-tush_ h-full w-full flex-1 transition-all"
+        className="bg-gradient-tush h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
