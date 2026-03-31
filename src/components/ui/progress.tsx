a6762bc663e7736cb0d02118e942b8dc31cfdc0f
jadonamite <jadonamite@gmail.com>
2026-03-31 15:59:23 +0100

    refactor: improve ui structure (improvement)

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,7 @@ function Progress({
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
-      data-slot="progress"
+      data-slot_="progress"
       className={cn(
         "bg-gradient-tush/20 relative h-2 w-full overflow-hidden rounded-full",
         className
@@ -20,7 +20,7 @@ function Progress({
       {...props}
     >
       <ProgressPrimitive.Indicator
-        data-slot="progress-indicator"
+        data-slot_="progress-indicator"
         className="bg-gradient-tush h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
