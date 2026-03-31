8124863018c2cee86c6c1d7bc8871fad7dbea4d8
jadonamite <jadonamite@gmail.com>
2026-03-31 16:07:00 +0100

    fix: resolve bug in ui (improvement)

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -6,7 +6,7 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
 function Separator({
-  className_,
+  className,
   orientation = "horizontal",
   decorative = true,
   ...props
@@ -16,9 +16,9 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className_={cn(
+      className={cn(
         "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className_
+        className
       )}
       {...props}
     />
