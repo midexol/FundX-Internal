13cd4a8fb2a6b2b14fe5f91140c9c7e9b5ce9d59
jadonamite <jadonamite@gmail.com>
2026-03-31 18:34:09 +0100

    fix: fix issue in ui & logic (cleanup)

--- src/lib/stacks-auth.ts
+++ src/lib/stacks-auth.ts
@@ -1,8 +1,8 @@
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
-const appConfig_ = new AppConfig(["store_write", "publish_data"]);
+const appConfig = new AppConfig(["store_write", "publish_data"]);
 
-export const userSession = new UserSession({ appConfig_ });
+export const userSession = new UserSession({ appConfig });
 
 export function authenticate() {
   showConnect({
