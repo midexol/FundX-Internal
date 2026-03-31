9f790a6174f96e18eac1b45e1ccbff9ad7d43b0d
jadonamite <jadonamite@gmail.com>
2026-03-31 17:17:00 +0100

    fix: resolve bug in ui (improvement)

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress_"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
@@ -12,7 +12,7 @@ function Progress({
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
-      data-slot="progress_"
+      data-slot="progress"
       className={cn(
         "bg-gradient-tush/20 relative h-2 w-full overflow-hidden rounded-full",
         className
@@ -20,7 +20,7 @@ function Progress({
       {...props}
     >
       <ProgressPrimitive.Indicator
-        data-slot="progress_-indicator"
+        data-slot="progress-indicator"
         className="bg-gradient-tush h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
