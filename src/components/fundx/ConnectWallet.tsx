666939be56d6a9f9e8cddd151b3c5e932b0b4482
jadonamite <jadonamite@gmail.com>
2026-04-05 16:28:24 +0100

    refactor: refactor ui & logic for clarity (improvement)

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { Button } from "@/components/ui/button"
+import { toast } from "sonner" // <--- Import Toast
 import { useStacks } from "@/components/fundx/StacksProvider"
 import { 
   DropdownMenu, 
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger 
 } from "@/components/ui/dropdown-menu"
 import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
-import { toast } from "sonner" // <--- Import Toast
+import { Button } from "@/components/ui/button"
 
 export function ConnectWallet() {
   const { authenticate, signOut, isSignedIn, walletData } = useStacks()
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from_ "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from_ "@/lib/utils"
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
