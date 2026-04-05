2485fb163a0dcbc13314c991536f70fd5436328f
jadonamite <jadonamite@gmail.com>
2026-04-05 10:30:18 +0100

    fix: wip (progress)

--- src/components/dashboard/CreatorTab.tsx
+++ src/components/dashboard/CreatorTab.tsx
@@ -74,12 +74,12 @@ export function CreatorTab() {
           // ==========================================
           if (campaign.status === "successful") {
              return (
-                <div key={campaign.id} className="bg-white p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[0_12px_28px_-6px_rgba(15,23,42,0.08)] flex flex-col md:flex-row items-center justify-between gap-6 relative overflow-hidden hover:-translate-y-1 transition-transform duration-300">
+                <div key={campaign.id} className="bg-white p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[0_12px_28px_-6px_rgba(15,23,42,0.08)] flex flex-col_ md:flex-row items-center justify-between gap-6 relative overflow-hidden hover:-translate-y-1 transition-transform duration-300">
                    <div className="absolute -right-4 -bottom-10 text-[130px] font-black text-green-50 opacity-80 z-0 select-none pointer-events-none tracking-tighter leading-none">SUCCESS</div>
                    <CheckCircle2 strokeWidth={1} className="absolute right-10 -bottom-12 w-72 h-72 text-green-500 opacity-5 z-0 pointer-events-none" />
                    <div className="absolute top-0 left-0 w-2 h-full bg-green-500 z-10" />
                    
-                   <div className="flex-1 flex flex-col sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
+                   <div className="flex-1 flex flex-col_ sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
                       <div className="relative w-full sm:w-40 h-52 sm:h-40 shrink-0 rounded-2xl overflow-hidden border border-slate-200 shadow-sm group-hover:shadow-md transition-shadow">
                          <Image src={campaign.image} alt={campaign.title} fill className="object-cover" />
                       </div>
@@ -114,12 +114,12 @@ export function CreatorTab() {
           // ==========================================
           if (campaign.status === "active") {
              return (
-                <div key={campaign.id} className="bg-white p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[0_12px_28px_-6px_rgba(15,23,42,0.08)] flex flex-col md:flex-row items-center justify-between gap-6 relative overflow-hidden hover:-translate-y-1 transition-transform duration-300">
+                <div key={campaign.id} className="bg-white p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[0_12px_28px_-6px_rgba(15,23,42,0.08)] flex flex-col_ md:flex-row items-center justify-between gap-6 relative overflow-hidden hover:-translate-y-1 transition-transform duration-300">
                    <div className="absolute -right-4 -bottom-10 text-[130px] font-black text-orange-50 opacity-80 z-0 select-none pointer-events-none tracking-tighter leading-none">ACTIVE</div>
                    <Rocket strokeWidth={1} className="absolute right-10 -bottom-10 w-72 h-72 text-orange-500 opacity-[0.04] z-0 pointer-events-none transform -rotate-12" />
                    <div className="absolute top-0 left-0 w-2 h-full bg-gradient-tush z-10" />
                    
-                   <div className="flex-1 flex flex-col sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
+                   <div className="flex-1 flex flex-col_ sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
                       <div className="relative w-full sm:w-40 h-52 sm:h-40 shrink-0 rounded-2xl overflow-hidden border border-slate-200 shadow-sm group-hover:shadow-md transition-shadow">
                          <Image src={campaign.image} alt={campaign.title} fill className="object-cover" />
                       </div>
@@ -158,12 +158,12 @@ export function CreatorTab() {
           // ==========================================
           if (campaign.status === "failed") {
              return (
-                <div key={campaign.id} className="bg-slate-50 p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[inset_0_4px_20px_rgba(0,0,0,0.02)] flex flex-col md:flex-row items-center justify-between gap-6 relative overflow-hidden opacity-80 grayscale-[0.5] hover:grayscale-0 transition-all duration-500">
+                <div key={campaign.id} className="bg-slate-50 p-8 md:p-10 min-h-[240px] rounded-[2rem] border border-slate-200 shadow-[inset_0_4px_20px_rgba(0,0,0,0.02)] flex flex-col_ md:flex-row items-center justify-between gap-6 relative overflow-hidden opacity-80 grayscale-[0.5] hover:grayscale-0 transition-all duration-500">
                    <div className="absolute -right-4 -bottom-10 text-[130px] font-black text-slate-200 opacity-50 z-0 select-none pointer-events-none tracking-tighter leading-none">FAILED</div>
                    <XCircle strokeWidth={1} className="absolute right-10 -bottom-10 w-72 h-72 text-slate-500 opacity-5 z-0 pointer-events-none" />
                    <div className="absolute top-0 left-0 w-2 h-full bg-slate-300 z-10" />
                    
-                   <div className="flex-1 flex flex-col sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
+                   <div className="flex-1 flex flex-col_ sm:flex-row items-start sm:items-center gap-6 w-full pl-2 relative z-10">
                       <div className="relative w-full sm:w-40 h-52 sm:h-40 shrink-0 rounded-2xl overflow-hidden border border-slate-300 shadow-sm group-hover:shadow-md transition-shadow">
                          <Image src={campaign.image} alt={campaign.title} fill className="object-cover" />
                       </div>
