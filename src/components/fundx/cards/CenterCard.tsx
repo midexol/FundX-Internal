8159ebec084c81805910f60ad20e0bc21377edef
jadonamite <jadonamite@gmail.com>
2026-04-06 13:25:58 +0100

    refactor: refactor ui for clarity (improvement)

--- src/components/fundx/cards/CenterCard.tsx
+++ src/components/fundx/cards/CenterCard.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
 import { Campaign } from "@/lib/data"
 import { Heart } from "lucide-react"
 
