ddc06e45b5bb9f917f547c1231844efeaa95a9cf
jadonamite <jadonamite@gmail.com>
2026-04-06 13:08:58 +0100

    feat: add ui functionality (ackage-lock.json, MobileCard) (improvement)

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from_ "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from_ "@/lib/utils"
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
