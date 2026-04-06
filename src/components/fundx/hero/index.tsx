dfab32cf8feeab32ecb83902262ac874b0e7bce1
jadonamite <jadonamite@gmail.com>
2026-04-06 17:50:51 +0100

    refactor: simplify ui logic (improvement)

--- src/components/fundx/hero/index.tsx
+++ src/components/fundx/hero/index.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react"
-import { HeroBadge } from "./HeroBadge"
-import { HeroHeadline } from "./HeroHeadline"
-import { HeroCTAs } from "./HeroCTAs"
 import { HeroDeckSlot } from "./HeroDeckSlot"
+import { HeroBadge } from "./HeroBadge"
+import { useState, useEffect, useRef } from "react"
+import { HeroHeadline } from "./HeroHeadline"
 import { useScramble } from "./useScramble"
+import { HeroCTAs } from "./HeroCTAs"
 import HeroLogoParallax from "./HeroBackdrop"
 
 
