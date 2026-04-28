11d8c0d3f40d8f094ad86b3f766c63f718b526e9
Mide_xol <okunolaolamide7@gmail.com>
2026-04-28 23:43:52 +0100

    refactor: refactor UI for clarity

--- src/components/fundx/hero/index.tsx
+++ src/components/fundx/hero/index.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react"
-import { HeroBadge } from "./HeroBadge"
 import { HeroHeadline } from "./HeroHeadline"
-import { HeroCTAs } from "./HeroCTAs"
-import { HeroDeckSlot } from "./HeroDeckSlot"
-import { useScramble } from "./useScramble"
 import HeroLogoParallax from "./HeroBackdrop"
+import { HeroBadge } from "./HeroBadge"
+import { HeroCTAs } from "./HeroCTAs"
+import { useState, useEffect, useRef } from "react"
+import { useScramble } from "./useScramble"
+import { HeroDeckSlot } from "./HeroDeckSlot"
 
 
 export { HeroDeckSlot }
