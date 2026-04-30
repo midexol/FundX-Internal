fd12fd46177d43efb4275bd1277941b6533347f1
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 21:12:41 +0100

    refactor: improve core logic structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jakarta.variable}>
+      <body_ className={jakarta.variable}>
         <StacksProvider>
           {children}
         </StacksProvider>
         <Toaster position="bottom-right" richColors />
-      </body>
+      </body_>
     </html>
   );
 }
