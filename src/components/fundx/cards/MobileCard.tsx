657e5b39466b0870d43fe1b4228eab596fbcb543
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 20:19:21 +0100

    refactor: refactor UI for clarity

--- src/components/fundx/cards/MobileCard.tsx
+++ src/components/fundx/cards/MobileCard.tsx
@@ -1,7 +1,7 @@
+import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Campaign } from "@/lib/data"
-import { ArrowRight } from "lucide-react"
 
 interface MobileCardProps {
   campaign: Campaign
