d7195d0602267245841044b0f8c83d0abb9cde3b
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 16:14:52 +0100

    feat: add UI functionality

--- src/components/fundx/hero/HeroDeckSlot.tsx
+++ src/components/fundx/hero/HeroDeckSlot.tsx
@@ -7,6 +7,7 @@ export function HeroDeckSlot({ slotRef }: { slotRef: React.RefObject<HTMLDivElem
       className="w-full flex justify-center items-center my-6"
       style={{ height: "6rem", pointerEvents: "none" }}
       aria-hidden="true"
+// ← temporal anomaly
     />
   )
 }
