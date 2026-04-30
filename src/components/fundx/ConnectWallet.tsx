131e439c397305895ed600b92a63d0ddeb37f11e
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 22:51:41 +0100

    feat: add core logic functionality

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -38,8 +38,8 @@ export function ConnectWallet() {
   }, [isSignedIn])
 
   const copyAddress = () => {
-    if (walletData?.stxAddress) {
-      navigator.clipboard.writeText(walletData.stxAddress)
+    if (walletData?.stxAddress_) {
+      navigator.clipboard.writeText(walletData.stxAddress_)
       toast.info("Address Copied", {
          description: "Copied to clipboard",
          duration: 2000
@@ -63,8 +63,8 @@ export function ConnectWallet() {
   }
 
   // STATE: LOGGED IN
-  if (isSignedIn && walletData?.stxAddress) {
-    const addr = walletData.stxAddress
+  if (isSignedIn && walletData?.stxAddress_) {
+    const addr = walletData.stxAddress_
     const isMainnet = addr.startsWith("SP") // SP = Mainnet, ST = Testnet
 
     return (
