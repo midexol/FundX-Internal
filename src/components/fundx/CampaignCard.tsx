a1bc706d80e629761044e8ed6bd5c6704a490818
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 14:24:21 +0100

    refactor: improve UI structure

--- src/components/fundx/CampaignCard.tsx
+++ src/components/fundx/CampaignCard.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image"
 import { Card } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 interface CampaignCardProps {
   id: string 
