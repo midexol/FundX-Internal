a599dddc0f28b9158ec9be6bf1bf4b7e2c34ca2c
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 23:58:42 +0100

    fix: fix issue in UI

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
-import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+
+import * as React from "react"
 
 function Tabs({
   className,
